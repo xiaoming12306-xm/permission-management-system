d52315060abd35a0d89af6b696b87d30c52e75b1
xiaoming12306-xm <1334857709@qq.com>
2025-03-04 10:46:21 +0800

    更改目录

--- main/permission-management-system/src/main/java/com/example/permissionsystem/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.permissionsystem.repository;
-
-import com.example.permissionsystem.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-
-  Permission findByName(String name);
-}
